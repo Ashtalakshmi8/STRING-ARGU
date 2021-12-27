d87ed32e8be3b5cb317df43294eadff1cbd2a2b6
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-28 03:08:56 +0530

    string argu-factrorial-series

---
+++ Factorialseries.java
@@ -0,0 +1,13 @@
+class Factorialseries{
+       public static void main(String[] args){
+             int no=9;
+             int fact=1;
+
+for (int i=1;i<=no;i++){
+{
+	fact=fact*i;
+       }
+	System.out.println( fact );	
+        }
+        }
+}
